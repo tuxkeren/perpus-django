840d26ae813c8bee5ca13705e4c181d4011232e4
Athailah <tuxkeren@gmail.com>
2025-05-03 21:38:01 +0700

    update code laporan export ke xlsx

--- perpustakaan/resource.py
+++ perpustakaan/resource.py
@@ -1,7 +1,12 @@
 from import_export import resources
 from perpustakaan.models import *
+from import_export.fields import Field
+
 
 class BukuResource(resources.ModelResource):
+	kelompok__nama = Field(attribute='kelompok', column_name='kelompok')
+	penerbit__nama = Field(attribute='penerbit', column_name='penerbit')
+
 	class Meta:
 		model = Buku
 		fields = ['judul','kelompok__nama','penulis','penerbit__nama','jumlah','tanggal']
